334323b2b6c9498f8258677fbc2ff22624286ba6
benedictekh <benedicte.kh@gmail.com>
2022-03-29 09:39:34 +0100

    Created face detection with image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import { AllUsersList } from "./components/allUsersList";
-import { CreateUserForm } from "./components/CreateUserForm";
+// import { AllUsersList } from "./components/allUsersList";
+// import { CreateUserForm } from "./components/CreateUserForm";
+import Recognition from "./components/Recognition";
 
 function App() {
   return (
     <div className="App">
-      <CreateUserForm></CreateUserForm>
+      {/* <CreateUserForm></CreateUserForm> */}
+      <Recognition></Recognition>
     </div>
   );
 }
---
+++ src/components/FaceApi.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const baseURL = "https://ambient-intelligence-group-3.cognitiveservices.azure.com/";
+const subscriptionKey = "86c96d069b2e48c891a35d82c9cc6ff7";
+const faceAttributes = "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+const detectionModel = "detection_01";
+export const faceApiForUrl = axios.create({
+    baseURL: baseURL,
+    timeout: 50000,
+    headers: {
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
+        "Content-Type": "application/json"
+    },
+    params: {
+        returnFaceId: true,
+        returnFaceLandmarks: false,
+        returnFaceAttributes: faceAttributes,
+        detectionModel: detectionModel
+    }
+});
+export const faceApiForUpload = axios.create({
+    baseURL: baseURL,
+    timeout: 50000,
+    headers: {
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
+        "Content-Type": "application/octet-stream"
+    },
+    params: {
+        returnFaceId: true,
+        returnFaceLandmarks: false,
+        returnFaceAttributes: faceAttributes,
+        detectionModel: detectionModel
+    }
+});
