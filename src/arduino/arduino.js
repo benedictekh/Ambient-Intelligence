49df8781473e840dc07f188a14553aa125615338
Inger-Ane Schefte <inger-aness@hotmail.com>
2022-03-31 10:50:07 +0100

    merge with main

--- src/arduino/arduino.js
+++ src/arduino/arduino.js
@@ -1,10 +1,6 @@
 const { Board, Led } = require("johnny-five");
-const { SerialPort } = require("serialport");
-const { ReadlineParser } = require("@serialport/parser-readline");
 
 const board = new Board({ port: "COM5" });
-
-//const ledModes = [red, yellow, blue];
 
 board.on("open", () => {
   console.log("serial port open");
@@ -25,19 +21,3 @@ function handleLedChange(mode) {
   console.log(mode);
   mode.on(500);
 }
-
-//handleLedChange(0);
-
-/*const port = new SerialPort({
-  path: "COM5",
-  baudRate: 9600,
-  //parser: new Readline({ delimeter: "\n" }),
-});*/
-//Port_#0001.Hub_#0001
-
-//const parser = board.pipe(new ReadlineParser({ delimiter: "\n" }));
-// Read the port data
-
-/*parser.on("data", (data) => {
-  console.log("got word from arduino:", data);
-});*/
--- src/components/User.js
+++ src/components/User.js
@@ -2,23 +2,24 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export function User(props) {
-    const [userPreference, setUserPreference] = useState([]);
+  const [userPreference, setUserPreference] = useState([]);
 
-    async function getUserPreference(name) {
-        axios.get(`http://localhost:5000/user/userDB/get/${name}`).then((response) => {
+  async function getUserPreference(name) {
+    axios
+      .get(`http://localhost:5000/user/userDB/get/${name}`)
+      .then((response) => {
         setUserPreference(response.data.user.preference);
-        
-        })
-    }
+      });
+  }
 
-    useEffect(() => {
-        getUserPreference(props.name);
-    }, []);
+  useEffect(() => {
+    getUserPreference(props.name);
+  }, []);
 
-    return(
-        <div>
-            <p>{props.name}</p>
-            <p>{userPreference}</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>{props.name}</p>
+      <p>{userPreference}</p>
+    </div>
+  );
 }
