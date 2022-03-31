755d55178cf8e0fa158f09cf764d3296e4da6053
Inger-Ane Schefte <inger-aness@hotmail.com>
2022-03-31 10:05:22 +0100

    wip

--- src/arduino/arduino.js
+++ src/arduino/arduino.js
@@ -1,35 +1,43 @@
-/*import React from "react";
-import ReactDOM from "react-dom";
-import five from "johnny-five";
-*/
-/*var board = new five.Board({
-  port: "COM5",
-});
-
-board.on("ready", function () {
-  var led = new five.Led(13);
-  led.blink(500);
-});*/
-
-// https://stackoverflow.com/questions/46994530/use-johnny-five-with-react
-
-//import SerialPort from "serialport";
-
+const { Board, Led } = require("johnny-five");
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
-const port = new SerialPort({
+
+const board = new Board({ port: "COM5" });
+
+//const ledModes = [red, yellow, blue];
+
+board.on("open", () => {
+  console.log("serial port open");
+});
+
+board.on("ready", () => {
+  console.log("hello");
+  const red = new Led(9);
+  const yellow = new Led(4);
+  const blue = new Led(13);
+
+  const ledModes = [red, yellow, blue];
+
+  handleLedChange(ledModes[1]);
+});
+
+function handleLedChange(mode) {
+  console.log(mode);
+  mode.on(500);
+}
+
+//handleLedChange(0);
+
+/*const port = new SerialPort({
   path: "COM5",
   baudRate: 9600,
   //parser: new Readline({ delimeter: "\n" }),
-});
+});*/
 //Port_#0001.Hub_#0001
-const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
+
+//const parser = board.pipe(new ReadlineParser({ delimiter: "\n" }));
 // Read the port data
 
-console.log("hellllooooo");
-port.on("open", () => {
-  console.log("serial port open");
-});
-parser.on("data", (data) => {
+/*parser.on("data", (data) => {
   console.log("got word from arduino:", data);
-});
+});*/
