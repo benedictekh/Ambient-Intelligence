76aabcb5186fa191f37613a58c58b8a454127394
benedictekh <benedicte.kh@gmail.com>
2022-04-04 19:18:10 +0100

    deleted old files that did not work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
-import CreateFaceGroup from "./components/CreateFaceGroup";
 import CreateUserGroup from "./components/CreateUserGroup";
 // import { AllUsersList } from "./components/allUsersList";
 // import { CreateUserForm } from "./components/CreateUserForm";
-import Recognition from "./components/old/Recognition";
 import { WebcamCapture } from "./components/TakePhoto2";
 // import TakePhoto from "./components/TakePhoto";
 
 function App() {
   return (
     <div className="App">
-      {/* <CreateUserForm></CreateUserForm> */}
-      {/* <Recognition></Recognition> */}
-      {/* <CreateFaceGroup></CreateFaceGroup> */}
-      {/* <TakePhoto></TakePhoto> */}
       <WebcamCapture></WebcamCapture>
       <CreateUserGroup></CreateUserGroup>
     </div>
