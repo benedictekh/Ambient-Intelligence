5cff692ea4b342f4635b840fb9ea23efe9fd4b28
benedictekh <benedicte.kh@gmail.com>
2022-04-06 10:26:27 +0100

    Clean up in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-// const { StrictEventEmitter } = require('socket.io/dist/typed-events');
-
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -20,36 +18,19 @@ app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    // You may need to change this in .ENV if the client starts on a different port.
     origin: local,
     methods: ["GET", "POST"],
   },
   maxHttpBufferSize: 1e8,
 });
 
-// const five = require('johnny-five');
-// const board = new five.Board();
-
-// let interval;
-
 io.on("connection", (socket) => {
   console.log("start" + socket.id);
   const id = socket.handshake.query.id;
   socket.join(id);
 
-  // if(interval) {
-  //     clearInterval(interval);
-  // }
-
-  // interval = setInterval(() => getApiAndEmit(socket), 1000);
-
   socket.on("start", (arg) => {
     console.log("start", arg);
-
-    // eventEmitter.emit('toggleLED');
-    // const red = new Led(13);
-
-    // handleLedChange(red)
   });
 
   socket.on("turnOnRed", (arg) => {
@@ -95,15 +76,8 @@ io.on("connection", (socket) => {
     servo.sweep();
   });
 
-  // socket.on('changeToBlue', (arg) => {
-  //     console.log("changeToBlue", arg);
-  //     const blue = new Led(13);
-  //     handleLedChange(blue)
-  // });
-
   socket.on("disconnect", () => {
     console.log("Client disconnected");
-    // clearInterval(interval);
     socket.off();
   });
 });
@@ -121,16 +95,8 @@ board.on("open", () => {
 
 board.on("ready", () => {
   console.log("hello");
-  // const red = new Led(9);
-  // const yellow = new Led(4);
-  // const blue = new Led(13);
-  // // const servo = new Servo(7);
-  // const ledModes = [red, yellow, blue];
-  // // servo.sweep();
-  // handleLedChange(ledModes[1]);
 });
 
 function handleLedChange(mode) {
-  // console.log(mode);
   mode.on(500);
 }
